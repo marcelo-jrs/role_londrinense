9b6b6f14a4c9561648c819890fbe0e44bc54915e
marcelin-jrs <fmarcelocarlos@gmail.com>
2023-08-30 12:12:48 -0300

    Initial commit / login e user group

---
+++ calendario/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Evento(models.Model):
+    id_evento = models.AutoField(primary_key=True)
+    nome_evento = models.TextField()
+    descricao = models.TextField()
+    data_inicio = models.DateTimeField()
+    data_final = models.DateTimeField()
+    local_online = models.IntegerField()
+    faixa_estaria = models.IntegerField()
+    id_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ calendario/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+
+
+# Create your views here.
+
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, 'home.html')
+    else:
+        return redirect('signin')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html',{
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request,
+            username=request.POST['username'],
+            password=request.POST['senha']
+        )
+
+        if user.is_authenticated:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Usuário ou senha incorretos'
+            })
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html',{
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['confirmarSenha'] == request.POST['senha']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                               first_name=request.POST['nome'],
+                                               last_name=request.POST['sobrenome'],
+                                               email=request.POST['email'],
+                                               password=request.POST['senha'])
+                user.save()
+                login(request, user)
+                return redirect('signin')
+            except:
+                return render(request, 'signup.html', {
+                            'form' : UserCreationForm,
+                            'error' : 'Usuário Já Existe'
+                            })
+            
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'As senhas são diferentes'
+        })
+
+def signout(request):
+    logout(request)
+    return redirect('signin')
