f26ef8da6d2b795e440439fc94cef8300ab86946
marcelin-jrs <fmarcelocarlos@gmail.com>
2023-09-21 16:57:39 -0300

    gunicorn config

---
+++ role_londrinense/gunicorn_config.py
@@ -0,0 +1,4 @@
+workers = 3  # Número de processos de trabalho
+bind = '0.0.0.0:8000'  # Endereço e porta para se ligar
+chdir = '/home/ubuntu/role_londrinense/role_londrinense'  # Diretório do projeto Django
+module = 'role_londrinense.wsgi:application'  # Caminho para a aplicação WSGI
